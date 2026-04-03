3b5f4a6f6c3812e70e49535b1c3f04a3a25b8ad5
Raz <amsalemraz10@gmail.com>
2026-04-03 15:05:47 +0300

    CONTINUE THE MISSION

--- pages/BooksDetails.jsx
+++ pages/BooksDetails.jsx
@@ -4,12 +4,24 @@ const { useState, useEffect } = React
 
 export function BooksDetails({ bookId, onBack }) {
     const [book, setBook] = useState(null)
+    const [count, pagesCount] = useState(null)
 
     useEffect(() => {
         bookService.get(bookId).then(fetchedBook => {
             setBook(fetchedBook)
         })
     }, [bookId])
+
+
+
+function pagesCount(count){
+    //TODO:
+// if (book.pageCount < 100) return 'Serious reading'
+// if (book.pageCount > 200) return 'Descent reading'
+// if (book.pageCount > 500) return 'Light reading'
+}
+
+
 
     if (!book) return <div>Loading...</div>
 
@@ -18,9 +30,10 @@ export function BooksDetails({ bookId, onBack }) {
             <h1>Book details</h1>
             <h3>ID: {book.id}</h3 >
             <h3>Title: {book.title}</h3 >
+            <h3>By: {book.authors}, {book.publishedDate}</h3 >
+            <h3>{book.pageCount} pages</h3 >
+            <h3>Price: {book.listPrice.amount} {book.listPrice.currencyCode}</h3>
             <h6>Description: {book.description}</h6 >
-            <h4>By: {book.authors} {book.publishedDate}</h4 >
-            <p>Price: {book.listPrice.amount} {book.listPrice.currencyCode}</p>
             <button onClick={onBack}>Back</button>
         </section >
     )

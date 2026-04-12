ebe4588cea604a655c83e9c53717378f5196e8fd
shaninagar9902 <shaninagar12345@gmail.com>
2026-04-12 14:28:52 +0300

    implement BookDetails component and review management

--- pages/BookDetails.jsx
+++ pages/BookDetails.jsx
@@ -29,9 +29,9 @@ export function BookDetails() {
 
     function getReadingType() {
         let pageCount = book.pageCount
-        if (pageCount >= 500) return '- Serious reading'
-        else if (pageCount >= 200) return '- Decent reading'
-        else if (pageCount <= 100) return '- Light reading'
+        if (pageCount >= 500) return 'Serious reading'
+        else if (pageCount >= 200) return 'Decent reading'
+        else if (pageCount <= 100) return 'Light reading'
         return 'Good reading'
     }
 
@@ -77,10 +77,14 @@ export function BookDetails() {
             <h1>Book details</h1>
             <h3>ID: {book.id}</h3 >
             <h3>Title: {book.title}</h3 >
-            <h3>By: {book.authors.join(',')}, {book.publishedDate} ({getPublish()})</h3 >
-            <h3>{book.pageCount} pages {getReadingType()}</h3 >
+            <h3>By: {book.authors.join(',')}, {book.publishedDate} {(getPublish())}</h3 >
+            <h3>{getReadingType()}: {book.pageCount} pages</h3 >
             <h3>Price: <span style={getAmount()}> {book.listPrice.amount} {book.listPrice.currencyCode}</span></h3>
             <h2>{onSale()}</h2>
+            <div className="description-container">
+                <h3>Description:</h3>
+                <LongTxt txt={book.description} length={100} />
+            </div>
             <div className="reviews">
                 <h3>Reviews</h3>
                 {!book.reviews || !book.reviews.length ? <p>No reviews yet</p> : <ul>
@@ -103,10 +107,6 @@ export function BookDetails() {
                     }} />
                 : <button onClick={() => setIsAddingReview(true)}>Add Review</button>
             }
-            <div className="description-container">
-                <h3>Description:</h3>
-                <LongTxt txt={book.description} length={100} />
-            </div>
             <div>
                 <Link to={`/book/${book.prevBookId}`}>Previous Book | </Link>
                 <Link to={`/book/${book.nextBookId}`}>Next Book</Link>
